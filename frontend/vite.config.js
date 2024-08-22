14ad2ac07b3698977e87585e9aff5d24a7b8c097
jhasudarshan <jha.microbit@gmail.com>
2024-08-23 01:35:05 +0530

    update config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:8000",
+        target: "https://chatapp-fhes.onrender.com",
         ws: true,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
