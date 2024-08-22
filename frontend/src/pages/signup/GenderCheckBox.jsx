325bf682e0904713932a6ea63040fe9e92ed210d
Sudarshan Kumar Jha <115224874+jhasudarshan@users.noreply.github.com>
2024-08-23 00:44:46 +0530

    Update GenderCheckBox.jsx

--- frontend/src/pages/signup/GenderCheckBox.jsx
+++ frontend/src/pages/signup/GenderCheckBox.jsx
@@ -3,7 +3,7 @@ const GenderCheckbox = (props) => {
 	return (
 		<div className='flex'>
 			<div className='form-control'>
-				<label className={`label gap-2 cursor-pointer`}>
+				<label className={`label gap-2 cursor-pointer ${selectedGender === "male" ? "selected" : ""} `}>
 					<span className='label-text'>Male</span>
 					<input 
 						type='checkbox' 
@@ -14,7 +14,7 @@ const GenderCheckbox = (props) => {
 				</label>
 			</div>
 			<div className='form-control'>
-				<label className={`label gap-2 cursor-pointer`}>
+				<label className={`label gap-2 cursor-pointer  ${selectedGender === "female" ? "selected" : ""}`}>
 					<span className='label-text'>Female</span>
 					<input 
 						type='checkbox' 
